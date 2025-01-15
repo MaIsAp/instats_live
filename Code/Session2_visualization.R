ddab792d25bc367de0f3c777a76c9ccfe557425c
Maria Isabel Arce <mi.arceplata@gmail.com>
2025-01-15 11:48:23 -0500

    Update Session2_visualization.R
    install first packages

--- Code/Session2_visualization.R
+++ Code/Session2_visualization.R
@@ -1,1 +1,11 @@
+# R code for visualizing data
 
+# Packages needed
+install.packages("terra")
+install.packages("devtools")
+
+library(terra)
+library(imageRy)
+library(devtools)
+# devtools related function to install packages from GitHub
+install_github("ducciorocchini/imageRy")
